edf4fbe1bede62c3078dfaa0d62ef28873f3475d
Madhur <madhurjain.kota@gmail.com>
2026-07-18 16:29:26 +0530

    add consumer

---
+++ consumer.js
@@ -0,0 +1,18 @@
+const {kafka} = require('./client');
+
+async function init(){
+    const consumer = kafka.consumer({groupId: "user-1"});
+
+    console.log('Connecting consumer');
+    await consumer.connect();
+    console.log('Consumer connected');
+
+    await consumer.subscribe({topics:["rider-updates"], fromBeginning:true})
+
+    await consumer.run({
+        eachMessage: async ({topic, partition, message, heartbeat, pause})=>{
+            console.log(`[${topic}]: PART:${partition}: `, message.value.toString())
+        }
+    })
+}
+init();
